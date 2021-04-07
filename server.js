d9fb9c65fc85be7112faf018fb34678d7177ba2e
jiangqiwen <jiangqwen@dingtalk.com>
2021-04-07 19:25:03 +0800

    版本

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ require('dotenv-flow').config();
 
 var server = require('@steedos/meteor-bundle-runner');
 var steedos = require('@steedos/core')
+
+// var express = require('express');
+// var app = express();
+
 server.Fiber(function () {
     try {
         server.Profile.run("Server startup", function () {
             server.loadServerBundles();
+
+            // var init = require('./lib/index').init;
+            // init({ app: app });
+            // WebApp.connectHandlers.use(app);
+
             steedos.init();
             server.callStartupHooks();
             server.runMain();
