ed5c38c5f617c5bd821caa02bdb29323d498216b
苏进荣 <jsingsu@163.com>
2021-04-27 19:53:07 +0800

    Revert "付款单中需对收票记录进行过滤，收款单中对开票记录进行过滤，只显示本合同的收票记录"
    This reverts commit 7295e90dc9f2a34c241849b91bf6a3a33766c344.

--- steedos-app/main/default/objects/contract_receipts/buttons/receipt.button.js
+++ steedos-app/main/default/objects/contract_receipts/buttons/receipt.button.js
@@ -1,17 +1,16 @@
 module.exports = {
-
-    receipt: function() {
-        Steedos.CRM.contractReceipt(this.record);
+    receipt: function(){
+      Steedos.CRM.contractReceipt(this.record);
     },
-    receiptVisible: function(object_name, record_id, record_permissions) {
-        // 如果是公司合同管理财务则显示出该按钮
-        var record = Creator.getObjectRecord(object_name, record_id);
-        var userId = Steedos.userId();
-        var roles = Creator.USER_CONTEXT.user.roles;
-        if (record && (roles.includes("contract_finance"))) {
-            return true;
-        }
-        return false;
+    receiptVisible: function (object_name, record_id, record_permissions) {
+      // 如果是公司合同管理财务则显示出该按钮
+      var record = Creator.getObjectRecord(object_name, record_id);
+      var userId = Steedos.userId();
+      var roles = Creator.USER_CONTEXT.user.roles;
+      if (record && (roles.includes("contract_finance"))) {
+        return true;
+      }
+      return false;
     }
-
-}
+  
+  }
