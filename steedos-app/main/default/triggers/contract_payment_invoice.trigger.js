0b325ccf9a1bd772ca2d5180fcabcf4f5c2c34a9
苏进荣 <jsingsu@163.com>
2021-04-27 19:54:29 +0800

    Revert "收票记录中的“发票类型”、“税率”及“发票齐全”字段删除，发票数和发票总金额无需分组，增加发票号，来源于收票明细中的发票号"
    This reverts commit dfe1f0121e3f119c7ed6a2aa827dbeaab6b423e3.

--- steedos-app/main/default/triggers/contract_payment_invoice.trigger.js
+++ steedos-app/main/default/triggers/contract_payment_invoice.trigger.js
@@ -11,30 +11,4 @@ module.exports = {
         }
     },
 
-    afterInsert: async function () {
-        await setInvoiceNumber(this.doc.payment_invoicefolder__c, this.spaceId);
-    },
-
-    afterDelete: async function () {
-        await setInvoiceNumber(this.previousDoc.payment_invoicefolder__c, this.spaceId);
-    },
-
-};
-
-// 计算收票记录的发票号
-async function setInvoiceNumber(paymentInvoiceFolderId, spaceId) {
-    if (!paymentInvoiceFolderId || !spaceId) {
-        return;
-    }
-    let steedosSchema = objectql.getSteedosSchema();
-    let invoiceNumbers = [];
-    let paymentInvoices = await steedosSchema.getObject('contract_payment_invoice').find({ filters: [['space', '=', spaceId], ['payment_invoicefolder__c', '=', paymentInvoiceFolderId]] });
-    for (const invoice of paymentInvoices) {
-        let num = invoice.invoice_number__c;
-        if (num) {
-            invoiceNumbers.push(num);
-        }
-    }
-    let numStr = invoiceNumbers.join(',');
-    await steedosSchema.getObject('contract_payment_invoicefolder').updateOne(paymentInvoiceFolderId, { 'invoice_number__c': numStr });
 }
