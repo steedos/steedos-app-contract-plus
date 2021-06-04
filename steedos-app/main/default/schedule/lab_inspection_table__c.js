65cc756d31f1072f547f46923cf469ecaed5e139
苏进荣 <jsingsu@163.com>
2021-06-04 15:54:31 +0800

    应用和机房巡检表增加相关字段DX代码上传、定时器增加默认字段以及名称增加默认值

--- steedos-app/main/default/schedule/lab_inspection_table__c.js
+++ steedos-app/main/default/schedule/lab_inspection_table__c.js
@@ -31,20 +31,27 @@ module.exports = {
         let space = await spaceObj.findOne(spaceId);
         if (space) {
           let now = new Date();
+          //let date = now.toLocaleDateString();
+          let year = now.getFullYear();
+          let month = now.getMonth() + 1;
+          month = month < 10 ? '0' + month : month;
+          let day = now.getDate();
+          day = day < 10 ? '0' + day : day;
+          let date = year + '-' + month + '-' + day;
           let owner = space.owner;
           let insertDoc = {
             'space': spaceId,
-            'owner': owner,
+            'owner': 'jeyoP8LKJXrkxE7hj',
             'created': now,
-            'created_by': owner,
+            'created_by': 'jeyoP8LKJXrkxE7hj',
             'air_conditioning__c': '正常', // 空调
             'air_summary__c': '', // 情况摘要
             'humidity__c': '正常', // 湿度
             'humidity_summary__c': '', // 情况摘要
-            'name': '', // 名称
+            'name': '机房巡检'+ date, // 名称
             'remarks__c': '', // （详细描述、有关现象、设备品牌、型号等）：
             'review_time__c': now, // 巡检时间
-            'reviewer__c': 'qEcJg5jat2oZCXTm3', // 巡检人
+            'reviewer__c': 'jeyoP8LKJXrkxE7hj', // 巡检人
             'router_port_label__c': '正常', // 交换机、路由器端口标签，网线标签，电源线标签齐全，清晰明了
             'router_port_summary__c': '', // 情况摘要
             'temperature__c': '正常', // 温度
@@ -62,6 +69,12 @@ module.exports = {
             'clean__c': '正常', // 干净整洁
             'clean_summary__c': '', // 情况摘要
             'detector__c': '正常', // 温湿检测
+            'detector_summary__c': '', // 情况摘要
+            'lighting__c': '正常', // 照明设施
+            'detector_summary__c': '', // 情况摘要
+            'ups__c': '正常', // ups配电
+            'detector_summary__c': '', // 情况摘要
+            'firefighting__c': '正常', // 消防设备
             'detector_summary__c': '', // 情况摘要
           };
           await labObj.insert(insertDoc);
